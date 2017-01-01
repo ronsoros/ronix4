2c0fd153b0ffb94ac1552241b8778d18fda7f3c8
ronsor <no-mail@nomail.lan>
2017-01-01 17:40:36 -0500

    add shim for linux users in kernel

--- kernel.c
+++ kernel.c
@@ -43,7 +43,93 @@ typedef struct fsparsed {
 	char path[64];
 } fsparsed;
 typedef int (*fsdriver)(enum fsact,int,int,char*,char*);
+#ifdef WIN32
+	#define NO_KBHIT_SHIM
+#endif
+#ifndef NO_KBHIT_SHIM
 
+
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios old, new;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new = old; /* make new settings same as old settings */
+  new.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void) 
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void) 
+{
+  return getch_(1);
+}
+#define _getche getche
+#define _getch getch
+ 
+void terminal_lnbuf(int yn) {
+   struct termios oldt, newt;
+   tcgetattr(0, &oldt);
+   newt = oldt;
+   if (!yn) newt.c_lflag &= ~ICANON;
+   else newt.c_lflag |= ICANON;
+   tcsetattr(0, TCSANOW, &newt);
+}
+ 
+void terminal_echo(int yn) {
+   struct termios oldt, newt;
+   tcgetattr(0, &oldt);
+   newt = oldt;
+   if (!yn) newt.c_lflag &= ~ECHO;
+   else newt.c_lflag |= ECHO;
+   tcsetattr(0, TCSANOW, &newt);
+}
+ 
+int kbhit() {
+   register int ret;
+   fd_set fds;
+   terminal_lnbuf(0);
+   terminal_echo(0);
+   struct timeval tv;
+   tv.tv_sec = 0;
+   tv.tv_usec = 0;
+   FD_ZERO(&fds);
+   FD_SET(0, &fds);
+   select(1, &fds, 0, 0, &tv);
+   ret = FD_ISSET(0, &fds);
+   terminal_lnbuf(1);
+   terminal_echo(1);
+   return ret;
+}
+#define _kbhit kbhit
+#endif
 #define newfsdriver(n) int n (enum fsact action, int seekn, int len, char *path, char *buf)
 #ifndef NO_FOPEN
 newfsdriver(fs_default){
